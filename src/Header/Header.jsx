68e402f2e25e9f48d068bb6b7ad8836a70ee0a7a
Tanish Singh <tanishsingh866@gmail.com>
2026-02-25 02:35:09 +0000

    Added navbar component using reactstrap

---
+++ src/Header/Header.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
+
+// CSS imports
+import './Header.css';
+
+function Header(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar {...props}>
+        <NavbarBrand href="/" id='title'>Shop Cart</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ms-auto" navbar>
+            <NavItem>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar style={{marginRight:"2rem"}}>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Cart</DropdownItem>
+                <DropdownItem>Settings</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Log Out</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavbarText>Username</NavbarText>
+          </Nav>
+          
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
